882bce40e3567c0815d48e21b68dd1bc4a1757fe
benleon <benleon@msn.com>
2020-08-10 16:16:00 -0700

    moar comments! hurray!

--- examples/snake/blocks.js
+++ examples/snake/blocks.js
@@ -2,11 +2,13 @@ function Block() {
     this.x;
     this.y;
 
+    // generates a random location for the block
     this.makeLoc = function() {
         this.x = (Math.floor(Math.random() * rows - 1) + 1) * scale;
         this.y = (Math.floor(Math.random() * columns - 1) + 1) * scale;
     }
 
+    // redraws the block.  called every interval
     this.draw = function() {
         ctx.fillStyle = "green";
         ctx.fillRect(this.x, this.y, scale, scale);
--- examples/snake/snake.js
+++ examples/snake/snake.js
@@ -1,11 +1,13 @@
 function Snake() {
     this.x = 0;
     this.y = 0;
+    // xSpeed is initialized so that the snake will start out by moving verttically to the right
     this.xSpeed = scale * 1;
     this.ySpeed = 0;
     this.length = 0;
     this.tail = [];
 
+    // redraw the snake and all of its tail, called every interval
     this.draw = function() {
         ctx.fillStyle = "#FFFFFF";
         for(let i=0; i<this.tail.length; i++){
@@ -14,6 +16,8 @@ function Snake() {
         ctx.fillRect(this.x, this.y, scale, scale);
     }
 
+    /* update the location of all blocks in the snake and its tail.  the tail is also updated by shifting all of
+        the blocks up by one index in the tail array */
     this.update = function() {
         for (let i=0; i<this.tail.length - 1; i++) {
             this.tail[i] = this.tail[i+1];
@@ -41,6 +45,7 @@ function Snake() {
         }
     }
 
+    // method that updates the speed variables of the snake based on the input from the user
     this.changeDirection = function(dir) {
         switch(dir) {
             case 'ArrowUp':
@@ -62,6 +67,7 @@ function Snake() {
         }
     }
 
+    // a function that is called when the snake head collides with a block, increasing the length of the snake
     this.eat = function(block) {
         if (this.x === block.x && this.y === block.y) {
             this.length++;
@@ -70,6 +76,7 @@ function Snake() {
         return false;
     }
 
+    // checks for game ending collisions in the body of the snake, and displays game over text if there is a colision
     this.checkCollision = function() {
         for (var i=0; i<this.tail.length; i++) {
             if(this.x === this.tail[i].x && this.y === this.tail[i].y) {
